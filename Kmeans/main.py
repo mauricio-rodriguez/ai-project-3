14047e4f3f0110d893fac1e54096fef52b2d79c0
mauricio-rodriguez <mauricio.rodriguez@utec.edu.pe>
2021-11-11 00:33:19 -0500

    carpeta

--- Kmeans/main.py
+++ Kmeans/main.py
@@ -1,13 +1,19 @@
-import preprocessing as p
-from kmeans import KMEANS
+# import preprocessing as p
+# from general_functions import helpers, preprocessing
+# from kmeans import KMEANS
+
+# if __name__ == "__main__":
+#     data = p.get_data("../dataset_tissue.txt")
+#     data = p.normalize(data)
+#     data = p.components_analysis(data)
+#     print(data.shape)
+#     print(data)
+#     # test = KMEANS(data,3)
+#     # test.kmeans()
+    
+from general_functions import helpers, preprocessing
+from data import reducted
 
 if __name__ == "__main__":
-    data = p.get_data("../dataset_tissue.txt")
-    data = p.normalize(data)
-    data = p.components_analysis(data)
-    print(data.shape)
-    print(data)
-    # test = KMEANS(data,3)
-    # test.kmeans()
     
     
